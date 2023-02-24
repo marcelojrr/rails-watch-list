eafca5bca1ad289315283eb9f1c613e887065563
Marcelo do Nascimento <junior69.mj@gmail.com>
2023-02-24 16:57:52 +0100

    delete_missing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
+  resources :lists do
+    resources :bookmarks
+  end
 end
